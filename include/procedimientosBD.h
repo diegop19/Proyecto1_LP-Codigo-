9930965c6b5a335da58a24f85e0e3675de48985e
chedrick343 <chedrickda014@estudiantec.cr>
2025-03-26 13:06:19 -0600

    Corrección de sintaxis e inclusión de un archivo .dll que necesitamos

--- include/procedimientosBD.h
+++ include/procedimientosBD.h
@@ -1,6 +1,6 @@
 #ifndef PROCEDIMIENTOSBD_H
 #define PROCEDIMIENTOSBD_H
-#include<mysql.h>
+#include <mysql.h>
 
 void insertarProducto(char *id, char *nombre, char *familia, float costo, float precio, int cantidad_stock);
 void insertarFamilia();
