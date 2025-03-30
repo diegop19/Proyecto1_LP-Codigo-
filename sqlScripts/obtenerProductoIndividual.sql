c80713680c0bee9a0e48033393b3019e8f0a4ee0
chedrick343 <chedrickda014@estudiantec.cr>
2025-03-29 18:09:51 -0600

    solo agregué el use

--- sqlScripts/obtenerProductoIndividual.sql
+++ sqlScripts/obtenerProductoIndividual.sql
@@ -1,10 +1,9 @@
+USE bazardb;
 DELIMITER $$
-
 CREATE PROCEDURE ObtenerProductoIndividual(IN idProducto VARCHAR(100))
 BEGIN
     SELECT nombreProducto, precio 
     FROM PRODUCTO
     WHERE identificadorProducto = idProducto;
 END $$
-
 DELIMITER ;
